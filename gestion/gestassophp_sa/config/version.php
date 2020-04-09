74c37af8df26c8fc09f263967e49304d594ed476
JCE <jc.etiemble@free.fr>
2020-04-09 17:46:39 +0200

    Version 7.6.4 - 09/04/2020 - Finale -------------------------------------- - Correction sur le Changelog.txt + lisez_moi.txt + Mise à jour versio

--- gestion/gestassophp_sa/config/version.php
+++ gestion/gestassophp_sa/config/version.php
@@ -21,7 +21,7 @@
 */
 
 /* La version de GestAsssoPhp  V X.X.S ou su dev V X.X.S-devx */
-$VERSION = "GestAssoPhp+Pg V 7.6.4-dev5";
+$VERSION = "GestAssoPhp+Pg V 7.6.4";
 define('VERSION', $VERSION);
 
 ?>
