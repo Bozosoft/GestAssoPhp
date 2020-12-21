3eb95ab1f36ab0483ee70941e6c0c812c4e3e5c7
JCE <jc.etiemble@free.fr>
2020-12-21 15:24:31 +0100

    Version 7.6.8 - 20/12/2020 - En cours (Dev2) ------------------------------------------- - Mise à jour de adodb à la version 5.20.19 du 13/12/2020

--- gestion/gestassophp_sa/config/version.php
+++ gestion/gestassophp_sa/config/version.php
@@ -21,7 +21,7 @@
 */
 
 /***** La version de GestAsssoPhp  V X.X ou dev V X.X-devx */
-$VERSION = "GestAssoPhp+Pg V 7.6.8-dev1";
+$VERSION = "GestAssoPhp+Pg V 7.6.8-dev2";
 define('VERSION', $VERSION);
 
 ?>
